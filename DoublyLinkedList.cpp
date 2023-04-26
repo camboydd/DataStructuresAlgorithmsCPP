14b2a27a818e84787a746e3df037c160b67f80ef
camboydd <98326906+camboydd@users.noreply.github.com>
2023-04-26 16:06:27 -0400

    Added more functions

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -63,6 +63,95 @@ class DoublyLinkedList{
             }
             length++;
         }
+
+        void deleteLast(){
+            if (length == 0) return;
+            Node* temp = tail;
+            if (length == 1){
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                tail = tail->prev;
+                tail->next = nullptr;
+            }
+            delete temp;
+            length--;
+        }
+
+        void prepend(int value){
+            Node* newNode = new Node(value);
+            if (length == 0){
+                head = newNode;
+                tail = newNode;
+            } else {
+                newNode->next = head;
+                head->prev = newNode;
+                head = newNode;
+            }
+            length++;
+        }
+
+        void deleteFirst(){
+            if (length == 0) return;
+            Node* temp = head;
+            if (length == 1){
+                head = nullptr;
+                tail = nullptr;
+            } else {
+                head = head->next;
+                head->prev = nullptr;
+            }
+            delete temp;
+            length--;
+        }
+
+        Node* get(int index){
+            if (index < 0 || index >= length)return nullptr;
+            Node* temp = head;
+            // Optimizing because it's doubly linked
+            if (index < length/2){
+                for (int i = 0; i < index; i++){
+                    temp = temp->next;
+                }
+            } else {
+                temp = tail;
+                for (int i = length-1; i > index; i--){
+                    temp = temp->prev;
+                }
+            }
+            return temp;
+
+        }
+
+        bool set(int index, int value){
+            Node* temp = get(index);
+            if (temp){
+                temp->value = value;
+                return true;
+            }
+            return false;
+        }
+
+        bool insert(int index, int value){
+            if (index < 0 || index > length) return false;
+            if (index == 0){
+                prepend(value);
+                return true;
+            }
+            if (index == length) {
+                append(value);
+                return true;
+            }
+            Node* newNode = new Node(value);
+            Node* before = get(index-1);
+            Node* after = before->next;
+            newNode->prev = before;
+            newNode->next = after;
+            before->next = newNode;
+            after->prev = newNode;
+            length++;
+            return true;
+        }
 };
 int main(){
     return 0;
